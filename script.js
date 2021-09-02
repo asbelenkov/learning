2f9d2704da8bcd3f8e7aad3c6180750364c2e663
asbelenkov <belenkov.spm@icloud.com>
2021-09-02 10:36:42 +0300

    date

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-let str = 'http://yandex.html';
-let foun = str.slice(0, 7);
-console.log(foun);
+let now = new Date();
+let date1 = new Date(now.getFullYear(), now.getMonth(),now.getDate() + 18);
+let date2 = new Date(now.getFullYear(), now.getMonth() + 1, now.getDate() + 8);
+let diff = date2 - date1;
+
+console.log(diff / 1000 / 60 / 60 / 24);
