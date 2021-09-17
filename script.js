1a5e4d4b20b0f2f3bc583397de93e18f0cb1626e
andreibelenkov <belenkov.spm@icloud.com>
2021-09-17 10:10:15 +0300

    Add column with remove row

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-let start = document.querySelector('#start');
-let stop = document.querySelector('#stop');
+let table = document.querySelector('table');
+let button_add_row = document.querySelector('#button_add_row');
+let button_add_column = document.querySelector('#button_add_column');
 
-start.addEventListener('click', function func() {
-    let i = 0;
+for (let i = 1; i <= 3; i++) {
+    let tr = document.createElement('tr');
 
-    let timerId = setInterval(() => {
-        console.log('!');
-    }, 1000)
+    for (let j = 1; j <= 3; j++) {
+        let td = document.createElement('td');
+        tr.appendChild(td);
+    }
+    table.appendChild(tr);
+
+};
+
+button_add_column.addEventListener('click', function func() {
+    let rows = document.querySelectorAll('tr');
+
+    for (let row of rows) {
+        let td = document.createElement('td');
+        row.appendChild(td);
+        button_add_column.removeEventListener('click', func);
+
+        let remove = document.createElement('a');
+        remove.href = '#';
+        remove.innerHTML = 'remove';
+        td.appendChild(remove);
+
+        remove.addEventListener('click', function(event) {
+            row.parentElement.removeChild(row);
+            event.preventDefault();
+        });
+
+    }
 });
 
-stop.addEventListener('click', function func() {});
+
